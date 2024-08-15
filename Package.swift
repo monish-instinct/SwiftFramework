cfa35a2f035c31dd568dbe3e2fdc1473940ce00d
InstincT <113701884+monish-instinct@users.noreply.github.com>
2024-08-15 22:34:29 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,20 +2,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "YourPackageName",
+    name: "ClFrameworkTest",
     platforms: [
         .iOS(.v13),
         .macOS(.v10_15),
     ],
     products: [
         .library(
-            name: "YourLibraryName",
-            targets: ["YourLibraryName"]),
+            name: "ClFrameworkTest",
+            targets: ["ClFrameworkTest"]),
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
-            name: "YourLibraryName",
+            name: "ClFrameworkTest",
             url: "https://github.com/monish-instinct/SwiftFramework/releases/download/swift/ClFrameworkTest.xcframework.zip",
             checksum: "6f8b10ec31407384b2c60316ee23c73d3bf8346e8ceb977761d65eac791bfe33"
         ),
